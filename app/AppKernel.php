619004f5d7f5ba89cb9825f61159561d1884d4b9
Anthony Boscardin <yayap13@gmail.com>
2015-03-25 15:09:01 +0100

    edit AppKernel timezone

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,7 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+	}
     
 	public function init()
     {
